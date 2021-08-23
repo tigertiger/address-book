fb3ed1f8ad5ecf8f44f7f057d60247aec0f054de
Melissa <melissa.scmi@gmail.com>
2021-08-23 10:31:14 -0700

    add functions to delete contacts/update names
    Co-authored by: Scott Hutley <scotthutley1@comcast.net>

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,21 @@ AddressBook.prototype.assignId = function() {
   return this.currentId;
 }
 
+AddressBook.prototype.findContact = function(id) {
+  if (this.contacts[id] != undefined) {
+    return this.contacts[id];
+  }
+  return false;
+};
+
+AddressBook.prototype.deleteContact = function(id) {
+  if (this.contacts[id] === undefined) {
+    return false;
+  }
+  delete this.contacts[id];
+  return true;
+};
+
 function Contact(firstName, lastName, phoneNumber, emailAddress) {
   this.firstName = firstName;
   this.lastName = lastName;
@@ -24,11 +39,23 @@ Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 };
 
+Contact.prototype.updateName = function(input) {
+  this.firstName = input;
+  return this.firstName;
+}
+
+// Contact.prototype.updateAll = function(input) {
+//   this.contact = input;
+//   return this.firstName;
+// }
 
 let addressBook = new AddressBook();
 let contact = new Contact("Ada", "Lovelace", "503-555-1111", "ada@geocities.com");
 let contact2 = new Contact("Grace", "Hopper", "503-555-0199", "grace@aol.com");
 addressBook.addContact(contact);
 addressBook.addContact(contact2);
+addressBook.deleteContact(1);
+addressBook.contacts;
 
+contact2.updateName("Lisa");
 
